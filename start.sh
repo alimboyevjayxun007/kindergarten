cc78e9602cdb56a0e464bdf00617911ae028a9bf
regellik <omon.pochta23@icloud.com>
2026-05-07 01:15:31 +0500

    Pin Python version and prepare SQLite startup

--- start.sh
+++ start.sh
@@ -3,12 +3,13 @@ set -o errexit
 
 SEED_MARKER="${SEED_ONCE_MARKER:-/var/data/.demo_seeded}"
 
+mkdir -p "$(dirname "$SEED_MARKER")"
+
 python manage.py migrate
 
 if [ ! -f "$SEED_MARKER" ]; then
   python manage.py seed_demo_data
   python manage.py setup_role_users
-  mkdir -p "$(dirname "$SEED_MARKER")"
   touch "$SEED_MARKER"
 fi
 
